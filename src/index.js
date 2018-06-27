da6f0c705f0182b11232e1c603a6f2413e3c3378
Jurek Barth <jurek.barth@virtual-identity.com>
2018-06-27 11:00:39 +0200

    add some style 💅

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './react-app/index.css';
-import registerServiceWorker from './react-app/registerServiceWorker';
-import {AppContainer} from "./react-app/AppContainer";
-import {Provider} from "react-redux";
-import {applyMiddleware, combineReducers, createStore} from "redux";
+import { AppContainer } from "./react-app/AppContainer";
+import { Provider } from "react-redux";
+import {
+	applyMiddleware,
+	combineReducers,
+	createStore
+} from "redux";
 import thunk from 'redux-thunk';
 
-const store = createStore(combineReducers({}), {loading: false, users: []}, applyMiddleware(thunk));
+const store = createStore(
+	combineReducers({}),
+	{ loading: false, users: [] },
+	applyMiddleware(thunk)
+);
 
 ReactDOM.render(
 	<Provider store={store}>
-		<AppContainer/>
+		<AppContainer />
 	</Provider>,
 	document.getElementById('root')
 );
-registerServiceWorker();
--- src/react-app/App.js
+++ src/react-app/App.js
@@ -3,14 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  renderList(users) {
+    return (
+      <ul class="users">
+        {users.map((user) => <li class="users__item">{user.name}</li>)}
+      </ul>
+    )
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-			{this.props.loading && <img src={logo} className="App-logo" alt="logo" />}
-            {this.props.users.map((user) => <img src="" alt={user.name}/>)}
+      <div className="app">
+        <header className="app__header">
+          <h1 className="app__title">Welcome to React</h1>
+          {this.props.loading && <div className="logo"><img src={logo} className="logo__image" alt="unicorn logo" /></div>}
         </header>
+        <div className="app__content">
+          {this.props.users.length > 0 ? this.renderList(this.props.users) : null}
+        </div>
+
+
       </div>
     );
   }
